c412339dc37797de14a954c8f086e35556eb0502
chrisstiliadis <cstiliadis@gmail.com>
2025-08-08 10:21:45 -0700

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,48 @@
+const commercialReel = [
+  {
+    "title": "Drake Nokia IMAX intro",
+    "credits": [
+      "Director: Theo Skudra",
+      "Music Production & Composition: Chris Stiliadis x Jimmy Nguyen",
+      "Mix: Chris Stiliadis"
+    ],
+    "videoUrl": "https://player.vimeo.com/video/975644071"
+  },
+  {
+    "title": "Drake Anita Max Win Tour",
+    "credits": [
+      "Director: Theo Skudra",
+      "Music Production & Composition: Chris Stiliadis x Jimmy Nguyen x William Lamoureux",
+      "Mix & Sound Design: Chris Stiliadis"
+    ],
+    "videoUrl": "https://player.vimeo.com/video/662704966"
+  },
+  {
+    "title": "Rove Concepts John Legend",
+    "credits": [
+      "Director: Theo Skudra",
+      "Sound Design & Mix: Chris Stiliadis",
+      "Music Direction: Chris Stiliadis x Jimmy Nguyen",
+      "Score: Jahan Sweet"
+    ],
+    "videoUrl": "https://player.vimeo.com/video/524965547"
+  },
+  {
+    "title": "Drake Mob Ties",
+    "credits": [
+      "Director: Theo Skudra",
+      "Music: Chris Stiliadis x Jimmy Nguyen",
+      "Mix & Sound Design: Chris Stiliadis"
+    ],
+    "videoUrl": "https://player.vimeo.com/video/524962950"
+  }
+];
+const artistPlaylistUrl = 'https://open.spotify.com/embed/playlist/2tZL2KmBep32gptkMH58Xs?utm_source=generator';
+const artistDescription = 'A compilation of songs I\'ve worked on – production, writing, engineering, mixing & mastering.';
+const podcastEmbedUrl = 'https://open.spotify.com/embed-podcast/show/5oGIAxBfnnWIdsgji1e0zQ?wmode=opaque';
+const podcastTitle = 'This Is not a Drake Podcast';
+const podcastSubtitle = 'CBC';
+const podcastDescription = 'Breaking down seminal moments in Drake\'s career to explore the broader history and evolution of hip-hop, R&B, gender dynamics and Black culture.';
+const podcastCredits = [
+  "Music Production & Composition: Chris Stiliadis"
+];
